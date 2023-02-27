f40aded9133da29ccefdce00f51f493f81d735a9
Nic <nicanderhery@gmail.com>
2023-02-27 22:58:43 +0100

    changed docs getStation

--- src/functions/getStations.ts
+++ src/functions/getStations.ts
@@ -2,13 +2,15 @@ import { Station } from "../structures/Station";
 
 /**
  * Returns all stations from the csv file
- * @returns {Promise<Station[]>}
+ * @returns {Promise<Station[]>} Array of Station objects
  */
 export const getStations = async () => {
     const csvToJson = require("csvtojson");
     const filepath =
         "./data/RMV_Haltestellen_Tarifperiode_2022_23_Stand_2022-12-21.csv";
     const stationsJSON = await csvToJson({ delimiter: ";" }).fromFile(filepath);
-    const stations = stationsJSON.map((station: any) => new Station(station));
+    const stations: Station[] = stationsJSON.map(
+        (station: any) => new Station(station)
+    );
     return stations;
 };
