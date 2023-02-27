52baa33d3405b9c259d99cb7feb0be259f1103f1
Nic <nicanderhery@gmail.com>
2023-02-27 23:42:07 +0100

    added date

--- src/structures/TransportWay.ts
+++ src/structures/TransportWay.ts
@@ -4,20 +4,26 @@ import { Transport } from "./Transport";
 export class TransportWay {
     departure: Station;
     destination: Station;
+    departure_date: string;
     departure_time: string;
+    arrival_date: string;
     arrival_time: string;
     using: Transport[];
 
     constructor(
         departure: Station,
         destination: Station,
+        departure_date: string,
         departure_time: string,
+        arrival_date: string,
         arrival_time: string,
         using: Transport[]
     ) {
         this.departure = departure;
         this.destination = destination;
+        this.departure_date = departure_date;
         this.departure_time = departure_time;
+        this.arrival_date = arrival_date;
         this.arrival_time = arrival_time;
         this.using = using;
     }
