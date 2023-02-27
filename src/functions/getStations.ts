715db6ab99b35cdf0dbf9716aec0fd73bd56423f
Nic <nicanderhery@gmail.com>
2023-02-27 23:15:34 +0100

    convert stations array into map

--- src/functions/getStations.ts
+++ src/functions/getStations.ts
@@ -2,15 +2,17 @@ import { Station } from "../structures/Station";
 
 /**
  * Returns all stations from the csv file
- * @returns {Promise<Station[]>} Array of Station objects
+ * @returns {<Promise<Map<number, Station>>>} Map with all stations
  */
 export const getStations = async () => {
     const csvToJson = require("csvtojson");
     const filepath =
         "./data/RMV_Haltestellen_Tarifperiode_2022_23_Stand_2022-12-21.csv";
     const stationsJSON = await csvToJson({ delimiter: ";" }).fromFile(filepath);
-    const stations: Station[] = stationsJSON.map(
-        (station: any) => new Station(station)
-    );
-    return stations;
+    const stationsMap = new Map<number, Station>();
+    stationsJSON.forEach((station: any) => {
+        const newStation = new Station(station);
+        stationsMap.set(newStation.hafas_id, newStation);
+    });
+    return stationsMap;
 };
