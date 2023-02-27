80dfe6825be200943f79c95bd9f9b869baac78c1
Nic <nicanderhery@gmail.com>
2023-02-27 21:02:12 +0100

    changed folder structure

--- src/importStation.ts
+++
@@ -1,8 +0,0 @@
-const csvToJson = require("csvtojson");
-const filepath =
-    "./data/RMV_Haltestellen_Tarifperiode_2022_23_Stand_2022-12-21.csv";
-
-export const getAllStations = async () => {
-    const stations = await csvToJson().fromFile(filepath);
-    return stations;
-};
